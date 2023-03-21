b8e23d3946cb78192064124196a30356bedd2da1
Iggynp <odissey0x@gmail.com>
2023-03-21 15:05:06 +0100

    added socials to navbar lg

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+function Home() {
+  return (
+	<div className='bg-[#0a192f] h-screen w-full'>
+
+	</div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaLinkedin, FaGithub} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/inp.jpeg'
 
 const Navbar = () => {
@@ -38,6 +40,33 @@ const Navbar = () => {
 			<li className='py-6 text-4xl'>Work</li>
 			<li className='py-6 text-4xl'>Contact</li>
 		</ul>
+
+		{/* social icons */}
+
+		<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+			<ul>
+				<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#00A0DC]'>
+					<a className='flex justify-between items-center w-full text-gray-300' href="/">
+						Linkedin <FaLinkedin size={30}/>
+					</a>
+				</li>
+				<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#313335]'>
+					<a className='flex justify-between items-center w-full text-gray-300' href="/">
+						Github <FaGithub size={30}/>
+					</a>
+				</li>
+				<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#DD5143]'>
+					<a className='flex justify-between items-center w-full text-gray-300' href="/">
+						Email <HiOutlineMail size={30}/>
+					</a>
+				</li>
+				<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#E68523]'>
+					<a className='flex justify-between items-center w-full text-gray-300' href="/">
+						Resume <BsFillPersonLinesFill size={30}/>
+					</a>
+				</li>
+			</ul>
+		</div>
 
 	</div>
 	
