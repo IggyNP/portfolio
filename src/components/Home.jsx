dd1153f4986be15d7d288900ba7cad39b8e1f651
Iggynp <odissey0x@gmail.com>
2023-03-21 16:21:12 +0100

    added Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 
-function Home() {
+const Home = () => {
   return (
-	<div className='bg-[#0a192f] h-screen w-full'>
-
+	<div name='home' className='bg-[#191825] h-screen w-full'>
+		<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center items-center h-full'>
+			<p className='text-l  sm:text-xl text-[#30E3CA]'>Hi! my name is</p>
+			<h1 className='text-4xl sm:text-7xl my-1 font-bold text-[#ccd6f6]'>Ignacio Navarro</h1>
+			<h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer.</h2>
+			<button className='text-[#ccd6f6] group border-2 px-6 py-3 my-4 flex items-center hover:bg-[#30E3CA] hover:border-[#30E3CA] hover:text-[#191825]'>View work 
+				<span className='group-hover:rotate-90 duration-300'>
+					<HiArrowNarrowRight className='ml-3'/>
+				</span>
+			</button>
+		</div>
 	</div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 	const handleClick = () => setNav(!nav);
 
   return (
-	<div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+	<div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#191825] text-gray-300'>
 		<div>
 			<img src={Logo} alt="Logo" style={{width: '50px'}} />
 		</div>
@@ -33,7 +33,7 @@ const Navbar = () => {
 
 		{/* mobile menu */}
 
-		<ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+		<ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#191825] flex flex-col justify-center items-center'}>
 			<li className='py-6 text-4xl'>Home</li>
 			<li className='py-6 text-4xl'>About</li>
 			<li className='py-6 text-4xl'>Skills</li>
