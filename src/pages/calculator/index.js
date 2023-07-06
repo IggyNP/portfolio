f1bdbf2e97eb7581d2226e7516dbcc77ab354fd4
Iggynp <odissey0x@gmail.com>
2023-07-07 00:11:10 +0200

    completed i18n on Calculator

--- src/pages/calculator/index.js
+++ src/pages/calculator/index.js
@@ -5,8 +5,11 @@ import Image from "next/image";
 import Calculator from "public/images/calculator.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const index = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Head>
@@ -40,18 +43,11 @@ const index = () => {
 
       <div className="mx-auto grid max-w-[1240px] grid-cols-4 gap-8 px-7 py-10">
         <div className="col-span-4 pr-10 md:col-span-3">
-          <p>Project</p>
-          <h2 className="font-bol py-2 text-3xl">Overview</h2>
-          <p>
-            The React Calculator is a beginner-level project that I created
-            during my early days of learning React. It is a simple calculator
-            application built using React and styled with CSS3. The calculator
-            provides basic arithmetic operations and allows users to perform
-            calculations in a user-friendly interface. The project showcases my
-            initial grasp of React concepts and my ability to develop and deploy
-            web applications. The React Calculator is deployed on Netlify,
-            making it accessible for demonstration purposes.
-          </p>
+          <p className="capitalize">{t("PROJECT_T1")}</p>
+          <h2 className="font-bol py-2 text-3xl capitalize">
+            {t("PROJECT_T2")}
+          </h2>
+          <p>{t("CALCULATOR_OVERVIEW")}</p>
           <div className="flex items-center justify-center pt-6 lg:pt-10">
             <Link
               href="https://github.com/IggyNP/react-calculator"
@@ -60,7 +56,7 @@ const index = () => {
               className="px-4"
             >
               <button className="hover: rounded border-2 bg-[#191825] bg-transparent px-6 py-2 uppercase shadow-md transition duration-150 ease-in-out hover:border-[#FC5185] hover:bg-[#FC5185] hover:text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#FC5185] dark:border-[#ccd6f6] dark:text-[#ccd6f6] dark:hover:border-[#30E3CA] dark:hover:bg-[#30E3CA] dark:hover:text-[#191825] dark:focus:ring-[#30E3CA]">
-                code
+                {t("PROJECT_CODE")}
               </button>
             </Link>
             <Link
@@ -80,26 +76,28 @@ const index = () => {
 
         <div className="col-span-4 rounded-xl py-4 shadow-sm shadow-gray-400 md:col-span-1">
           <div className="p-2">
-            <p className="pb-2 text-center font-bold">Technologies</p>
+            <p className="pb-2 text-center font-bold">
+              {t("PROJECT_TECHNOLOGIES")}
+            </p>
             <div className="grid grid-cols-3 justify-end md:w-full md:grid-cols-1 md:pl-4 lg:pl-20">
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> React
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> react
               </p>
               <p className="flex items-center py-2 pl-2 text-gray-600">
                 <RiRadioButtonFill size={15} className="pr-1" /> CSS3
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Javascript
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> javascript
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Netlify
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> netlify
               </p>
             </div>
           </div>
         </div>
         <Link href="/#work">
-          <p className="cursor-pointer underline hover:text-[#FC5185] dark:hover:text-[#30E3CA]">
-            Back
+          <p className="cursor-pointer capitalize underline hover:text-[#FC5185] dark:hover:text-[#30E3CA]">
+            {t("PROJECT_BACK")}
           </p>
         </Link>
       </div>
@@ -108,3 +106,12 @@ const index = () => {
 };
 
 export default index;
+
+export async function getStaticProps(context) {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
