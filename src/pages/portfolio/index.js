4f8f5f25e89d0ce3bed3b14e5b1667278a3f3efa
Iggynp <odissey0x@gmail.com>
2023-07-07 00:25:09 +0200

    completed i18n on Portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -5,8 +5,11 @@ import Image from "next/image";
 import portfolio2 from "public/images/portfolio2.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const index = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Head>
@@ -40,20 +43,11 @@ const index = () => {
 
       <div className="mx-auto grid max-w-[1240px] grid-cols-4 gap-8 px-7 py-10">
         <div className="col-span-4 pr-10 md:col-span-3">
-          <p>Project</p>
-          <h2 className="font-bol py-2 text-3xl">Overview</h2>
-          <p>
-            My portfolio is a showcase of my passion for front-end development.
-            With a focus on crafting visually compelling and user-friendly web
-            applications, I employ Next.js and Tailwind CSS to bring my projects
-            to life. From dynamic e-commerce solutions to interactive web
-            experiences, my portfolio reflects my commitment to delivering
-            seamless and engaging user interfaces. By leveraging the power of
-            modern web technologies, I strive to create intuitive designs that
-            leave a lasting impact. Explore my portfolio to witness examples of
-            my front-end development skills and discover the captivating
-            websites I've created.
-          </p>
+          <p className="capitalize">{t("PROJECT_T1")}</p>
+          <h2 className="font-bol py-2 text-3xl capitalize">
+            {t("PROJECT_T1")}
+          </h2>
+          <p>{t("PORTFOLIO_OVERVIEW")}</p>
           <div className="flex items-center justify-center pt-6 lg:pt-10">
             <Link
               href="https://github.com/IggyNP/portfolio"
@@ -62,7 +56,7 @@ const index = () => {
               className="px-4"
             >
               <button className="hover: rounded border-2 bg-[#191825] bg-transparent px-6 py-2 uppercase shadow-md transition duration-150 ease-in-out hover:border-[#FC5185] hover:bg-[#FC5185] hover:text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#FC5185] dark:border-[#ccd6f6] dark:text-[#ccd6f6] dark:hover:border-[#30E3CA] dark:hover:bg-[#30E3CA] dark:hover:text-[#191825] dark:focus:ring-[#30E3CA]">
-                code
+                {t("PROJECT_CODE")}
               </button>
             </Link>
           </div>
@@ -72,29 +66,31 @@ const index = () => {
 
         <div className="col-span-4 rounded-xl py-4 shadow-sm shadow-gray-400 md:col-span-1">
           <div className="p-2">
-            <p className="pb-2 text-center font-bold">Technologies</p>
+            <p className="pb-2 text-center font-bold capitalize">
+              {t("PROJECT_TECHNOLOGIES")}
+            </p>
             <div className="grid grid-cols-3 justify-end md:w-full md:grid-cols-1 md:pl-4 lg:pl-20">
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Next.js
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> next.js
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> React
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> react
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Tailwind
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> tailwind
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Javascript
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> javascript
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Netlify
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> netlify
               </p>
             </div>
           </div>
         </div>
         <Link href="/#work">
-          <p className="cursor-pointer underline hover:text-[#FC5185] dark:hover:text-[#30E3CA]">
-            Back
+          <p className="cursor-pointer capitalize underline hover:text-[#FC5185] dark:hover:text-[#30E3CA]">
+            {t("PROJECT_BACK")}
           </p>
         </Link>
       </div>
@@ -103,3 +99,12 @@ const index = () => {
 };
 
 export default index;
+
+export async function getStaticProps(context) {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
