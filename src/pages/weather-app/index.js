f96988e50cf4693afd8406480c671f228304b01d
Iggynp <odissey0x@gmail.com>
2023-07-07 16:47:10 +0200

    completed i18n on Weather page

--- src/pages/weather-app/index.js
+++ src/pages/weather-app/index.js
@@ -5,8 +5,11 @@ import Image from "next/image";
 import WeatherApp from "public/images/weather-app.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const index = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Head>
@@ -42,17 +45,11 @@ const index = () => {
 
       <div className="mx-auto grid max-w-[1240px] grid-cols-4 gap-8 px-7 py-10">
         <div className="col-span-4 pr-10 md:col-span-3">
-          <p>Project</p>
-          <h2 className="font-bol py-2 text-3xl">Overview</h2>
-          <p>
-            The Weather App is a classic learner project that I developed using
-            React, Tailwind CSS, and RapidAPI. This application allows users to
-            search for weather information of different locations. By leveraging
-            the RapidAPI platform, the app retrieves real-time weather data and
-            presents it in a user-friendly interface. It's important to note
-            that the app operates within certain limitations, as it utilizes the
-            free tier of RapidAPI, allowing a total of 100 requests per month.
-          </p>
+          <p className="capitalize">{t("PROJECT_T1")}</p>
+          <h2 className="font-bol py-2 text-3xl capitalize">
+            {t("PROJECT_T2")}
+          </h2>
+          <p>{t("WEATHER_OVERVIEW")}</p>
           <div className="flex items-center justify-center pt-6 lg:pt-10">
             <Link
               href="https://github.com/IggyNP/weather-app"
@@ -61,7 +58,7 @@ const index = () => {
               className="px-4"
             >
               <button className="hover: rounded border-2 bg-[#191825] bg-transparent px-6 py-2 uppercase shadow-md transition duration-150 ease-in-out hover:border-[#FC5185] hover:bg-[#FC5185] hover:text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#FC5185] dark:border-[#ccd6f6] dark:text-[#ccd6f6] dark:hover:border-[#30E3CA] dark:hover:bg-[#30E3CA] dark:hover:text-[#191825] dark:focus:ring-[#30E3CA]">
-                code
+                {t("PROJECT_CODE")}
               </button>
             </Link>
             <Link
@@ -81,29 +78,31 @@ const index = () => {
 
         <div className="col-span-4 rounded-xl py-4 shadow-sm shadow-gray-400 md:col-span-1">
           <div className="p-2">
-            <p className="pb-2 text-center font-bold">Technologies</p>
+            <p className="pb-2 text-center font-bold">
+              {t("PROJECT_TECHNOLOGIES")}
+            </p>
             <div className="grid grid-cols-3 justify-end md:w-full md:grid-cols-1 md:pl-4 lg:pl-20">
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> React
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> react
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Tailwind CSS
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> tailwind CSS
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Javascript
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> javascript
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Rapid API
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> rapid API
               </p>
-              <p className="flex items-center py-2 pl-2 text-gray-600">
-                <RiRadioButtonFill size={15} className="pr-1" /> Netlify
+              <p className="flex items-center py-2 pl-2 capitalize text-gray-600">
+                <RiRadioButtonFill size={15} className="pr-1" /> netlify
               </p>
             </div>
           </div>
         </div>
         <Link href="/#work">
-          <p className="cursor-pointer underline hover:text-[#FC5185] dark:hover:text-[#30E3CA]">
-            Back
+          <p className="dark:hover:text-[#30E3CA cursor-pointer capitalize underline hover:text-[#FC5185]">
+            {t("PROJECT_BACK")}
           </p>
         </Link>
       </div>
@@ -112,3 +111,12 @@ const index = () => {
 };
 
 export default index;
+
+export async function getStaticProps(context) {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
