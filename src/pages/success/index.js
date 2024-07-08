8089712059786806ca07f028e3f9dc4f33e9ca3f
Iggynp <odissey0x@gmail.com>
2024-07-08 16:20:48 +0200

    i18next for form success

--- src/pages/success/index.js
+++ src/pages/success/index.js
@@ -2,25 +2,28 @@ import React from "react";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const success = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Head>
-        <title>IggyNP | Success</title>
+        <title>IggyNP | {t("SUCCESS_TITLE")}</title>
       </Head>
       <Navbar showSocials={true} />
       <div id="success" className="h-screen w-full">
         <div className="mx-auto flex h-full max-w-[1000px] flex-col items-center justify-center px-8 text-center">
           <h1 className="text-4xl  text-[#FC5185] dark:text-[#30E3CA] sm:text-6xl">
-            Thank you for your message!
+            {t("THANKS_FOR_YOUR_MESSAGE")}
           </h1>
           <h2 className="my-2 text-xl font-bold sm:my-4 sm:text-2xl">
-            I'll get back to you as soon as possible.
+            {t("SUCCESS_MESSAGE")}
           </h2>
           <Link href="/#contact" scroll={false}>
             <button className="group mt-6 flex items-center rounded border-2 px-6 py-3 uppercase shadow-md transition duration-300 ease-in-out hover:border-[#FC5185] hover:bg-[#FC5185] hover:text-white dark:hover:border-[#30E3CA] dark:hover:bg-[#30E3CA] dark:hover:text-[#191825]">
-              return
+              {t("PROJECT_BACK")}
             </button>
           </Link>
         </div>
@@ -30,3 +33,12 @@ const success = () => {
 };
 
 export default success;
+
+export async function getStaticProps(context) {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
